044d80bd41a6553831274d0136c6f1ec7b0f5585
ZezetuSibara <Zezethu0730@gmail.com>
2023-02-21 13:35:31 -0800

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Point of entry
+ * _putchar - Prints out utchar letters
+ *
+ * Return: On Success 0.
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
