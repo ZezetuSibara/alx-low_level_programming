b61e3304d825d17b973a5a5b5729a59faecd4252
ZezetuSibara <Zezethu0730@gmail.com>
2023-02-21 14:27:46 -0800

    2-Print_Alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for (i = 1; i <= 10; i++)
+	for (i =  1; i <= 10 ; i++)
 	{
-		for (j = '1'; j <= 'z'; j++)
+		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
