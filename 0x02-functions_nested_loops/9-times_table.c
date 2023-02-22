8999a67412fec4853ea9664f5c74983fd45bf327
ZezetuSibara <Zezethu0730@gmail.com>
2023-02-22 03:23:41 -0800

    9-Times_Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,12 @@ void times_table(void)
 {
 	int x, y, z, u, d;
 
-	for (x = 0 ; x < 10 ; x++)
+	for (x = 0 ; x <= 9 ; x++)
 	{
-		for (y = 0 ; y < 10 ; y++)
+		for (y = 0 ; y <= 9 ; y++)
 		{
 			z = x * y;
-			if (z > 10)
+			if (z > 9)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
