af4708b17b40e1907fcdd41dc4bfd5e8eb11025f
ZezetuSibara <Zezethu0730@gmail.com>
2023-03-14 11:04:28 -0700

    101-Strtow

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 int main(int ac, char *av[]);
+int count_word(char *s);
+char **strtow(char *str);
 
 #endif
