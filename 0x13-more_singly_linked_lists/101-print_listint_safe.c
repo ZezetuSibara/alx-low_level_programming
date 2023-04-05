e439b000642110c322ed24f24cd70d23db03575b
ZezetuSibara <Zezethu0730@gmail.com>
2023-04-05 15:52:36 -0700

    101-Print_ListInt_Safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,53 +5,53 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
+ * looped_listint_len - number of unique nodes
+ * in a looped linked list.
+ * @head: A pointer to the head to check.
+ * Return: 0 if the list is not looped
  * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
+	const listint_t *dog, *cat;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	dog = head->next;
+	cat = (head->next)->next;
 
-	while (hare)
+	while (cat)
 	{
-		if (tortoise == hare)
+		if (dog == cat)
 		{
-			tortoise = head;
-				while (tortoise != hare)
+			dog = head;
+				while (dog != cat)
 			{
 				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
+				dog = dog->next;
+				cat = cat->next;
 			}
-			tortoise = tortoise->next;
+			dog = dog->next;
 
-			while (tortoise != hare)
+			while (dog != cat)
 			{
 				nodes++;
-				tortoise = tortoise->next;
+				dog = dog->next;
 			}
 			return (nodes);
 		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		dog = dog->next;
+		cat = (cat->next)->next;
 	}
 	return (0);
 }
 
 /**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
- * Return: The number of nodes in the list.
+ * print_listint_safe - Prints a list safely.
+ * @head: A pointer to the list head.
+ * Return: The number of nodes.
  */
 size_t print_listint_safe(const listint_t *head)
 {
