7b16702adcac19688fb50b0ea9cb56c94354ea67
ZezetuSibara <zezethu0730@gmail.com>
2023-06-19 16:15:41 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,17 +1,3 @@
-#include <stdlib.h>
-#include <unistd.h>
-
-/**
- * srand - the srand function within the libc shared object
- * @seed: To use with the random number generator. Will be Ignored
- */
-void srand(unsigned int seed)
-{
-	const char *nums_txt = "9 8 10 24 75 - 9\n";
-	const char *txt = "Congratulations, you win the Jackpot!\n";
-
-	(void)seed;
-	write(STDOUT_FILENO, (void *)nums_txt, 17);
-	write(STDOUT_FILENO, (void *)txt, 38);
-	exit(EXIT_SUCCESS);
-}
+#!/bin/bash
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/ZezetuSibara/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
